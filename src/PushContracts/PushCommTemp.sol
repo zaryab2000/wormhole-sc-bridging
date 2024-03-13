be16845dcead5abd6e1505e3f11daa88074609db
Zaryab <zaryabafser2000@gmail.com>
2024-03-13 10:01:06 +0400

    added custom push token

--- src/PushContracts/PushCommTemp.sol
+++ src/PushContracts/PushCommTemp.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import {TokenBase, TokenSender} from "wormhole-solidity-sdk/WormholeRelayerSDK.sol";
 import "wormhole-solidity-sdk/interfaces/IERC20.sol";
